10503082d6dbd1712ccd0ab064430f6f6b4c984f
hemant09 <hemantsharma0903@gmail.com>
2016-01-06 18:18:45 -0800

    student database

---
+++ json.js
@@ -0,0 +1,23 @@
+student database {
+
+  "student":  {"firstName":"Hemant", "lastName":"Sharma"},
+
+  "college": "MRIU",
+  "course": "B.tech",
+  "stream": "Computer Science",
+  "roll no": "1/12/FET/BCS/2/115",
+  "address": "Delhi",
+  "phone number":"9654399450",
+  "cgpa": "6.6",
+  "fav sports":"Football"
+
+"departments": [
+                  {"department":"CSE"},
+                  {"department":"IT"}
+    ]
+"subject list": [
+        {"Subject": "AI", "code": "111", "coordinator":"Ram" },
+        {"Subject": "SPM", "code": "121", "coordinator":"Grv"},
+        {"Subject": "ST", "code": "131", "coordinator":"Mohit"}
+    ]
+}
