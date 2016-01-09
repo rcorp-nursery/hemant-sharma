d119b8c871735e9eb92d00ff3f76e3e16baeb658
hemant09 <hemantsharma0903@gmail.com>
2016-01-09 10:41:19 -0800

    add more field

--- json.js
+++ json.js
@@ -1,23 +1,77 @@
-student database {
+college student database {
 
-  "student":  {"firstName":"Hemant", "lastName":"Sharma"},
+  "college info": {
+      "name": {"name":"Manav Rachna", "university": "MRU"},
+      "course name": {"b.tech", "m.tech", "mba", "bpt", "dental"},
+      "sports name": {"football","basketball","cricket","hockey","tennis"},
+      "departments": {"cse","it","ece","civil","mech","aero"}},
 
-  "college": "MRIU",
+  "college fee": {"b.tech": "1,23,000", "m.tech": "1,50,000", "mba": "2,00,000",
+                 "bpt": "2,10,000", "dental": "3,90,000"},
+
+  "course faculty": {"cse": "a block", "mech": "c block", "bpt": "b block",
+                      "mba": "e block", "dental": "l block"},
+
+  "extra activities name": {"drama", "workshops", "dance", "events"
+                                 "gd pro", "musicfests"},
+
+  "placement companies": {"tcs":{"tcs address":"noida"},
+                      "ericsson", "pyverse", "collebra"},
+
+  "college time": [{"morning time": "8 a.m-2 p.m"},
+                   {"evening time": "11 a.m-5 p.m"}],
+
+  "student": {"Name":{"name": "Hemant":{
+             "roll number": "115",
+             "id": "03",
+             "age": "21",
+             "gender": "male",
+             "alive": "true",
+             "siblings": [{
+                    "name":  "Armaan"
+                    "age":   "24"
+                    "address": "delhi"},
+
+          {         "name":  "Keshav",
+                    "age":   "22"
+                    "address": "fbd"}]
+  } }, "lastName": "Sharma"}},
+
   "course": "B.tech",
   "stream": "Computer Science",
+  "section": "D",
+  "group": "d2",
   "roll no": "1/12/FET/BCS/2/115",
+  "final year project": "encrypted image trasnmission through cloud",
+  "project group": "g6",
+  "project status": "accepted",
+  "project start date": "12 july 2015",
+  "project submission date": "07 jan 2016",
   "address": "Delhi",
+  "d.o.b": "03/12/1993",
   "phone number":"9654399450",
-  "cgpa": "6.6",
-  "fav sports":"Football"
+  "cgpa sem wise":
 
-"departments": [
-                  {"department":"CSE"},
-                  {"department":"IT"}
-    ]
-"subject list": [
-        {"Subject": "AI", "code": "111", "coordinator":"Ram" },
-        {"Subject": "SPM", "code": "121", "coordinator":"Grv"},
-        {"Subject": "ST", "code": "131", "coordinator":"Mohit"}
-    ]
-}
+  [
+           {"1st sem": "5.8"}, {"2nd sem": "6.2"},
+           {"3rd sem": "6.7"}, {"4th sem": "6.4"},
+           {"5th sem": "6.0"}, {"6th sem": "6.3"}],
+
+  "fav sports":"Football",
+  "department": "CSE",
+  "fav superhero": "batman":
+                            {"real name": "bruce wayne",
+                            "address": "gotham", "powers":"detective mind",
+                            "enemy":{"joker","bane","deathstroke"
+                          }}
+
+  "subject list": [
+        {"Subject": "AI", "code": "111",
+        "coordinator":"professor":{"name":"ram", "address": "delhi"}},
+        {"Subject": "SPM", "code": "121",
+        "coordinator":"professor":{"name":"grv", "address": "delhi"}},
+        {"Subject": "ST", "code": "131",
+        "coordinator":"professor":{"name":"mohit", "address": "delhi"}],
+
+  "internship details": [{"name": "Rcorp":{"address": "hauzkhas",}},   ]
+        }
